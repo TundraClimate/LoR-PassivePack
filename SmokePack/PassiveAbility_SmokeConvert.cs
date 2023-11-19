d58c63b271415d980fba85cccffa0a800d9629ea
TundraClimate <tundra.tundere@gmail.com>
2023-11-19 23:57:08 +0900

    feat: added SmokeConvert Passive

---
+++ SmokePack/PassiveAbility_SmokeConvert.cs
@@ -0,0 +1,14 @@
+﻿public class PassiveAbility_SmokeConvert : PassiveAbilityBase
+{
+  public override void OnRoundStart()
+  {
+    BattleUnitBuf smoke = base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+    if (smoke != null)
+    {
+      int divided = (smoke.stack + 1) / 2;
+      base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Strength, divided, base.owner);
+      base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Endurance, divided, base.owner);
+      base.owner.bufListDetail.RemoveBuf(base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke));
+    }
+  }
+}
