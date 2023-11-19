d569b9cfa58bcf891bbeeacf867e80bf53fcfdb1
TundraClimate <tundra.tundere@gmail.com>
2023-11-19 22:08:15 +0900

    feat: SmokePack initial

---
+++ SmokePack/PassiveAbility_SmokeDraw.cs
@@ -0,0 +1,22 @@
+﻿public class PassiveAbility_SmokeDraw : PassiveAbilityBase
+{
+  private bool _alreadyUsed = false;
+
+  public override void OnRoundStart()
+  {
+	this._alreadyUsed = false;
+  }
+
+  public override void OnSucceedAttack(BattleDiceBehavior behavior)
+  {
+	if (behavior.card == null || behavior.card.target == null) return;
+	if (this._alreadyUsed) return;
+	BattleUnitModel target = behavior.card.target;
+	BattleUnitBuf targetSmoke = target.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+	if (targetSmoke != null && targetSmoke.stack >= 5)
+	{
+	  base.owner.allyCardDetail.DrawCards(1);
+	  this._alreadyUsed = true;
+	}
+  }
+}
