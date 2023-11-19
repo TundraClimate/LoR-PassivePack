089d5cdf1d3e97a8e72b17286bfebc1fbf4c8d41
TundraClimate <tundra.tundere@gmail.com>
2023-11-20 07:55:51 +0900

    feat: added SmokeDmgBufAndBind Passive

---
+++ SmokePack/PassiveAbility_SmokeDmgBufAndBind.cs
@@ -0,0 +1,23 @@
+﻿public class PassiveAbility_SmokeDmgBufAndBind : PassiveAbilityBase
+{
+  private int _smokeCount = 0;
+
+  public override void OnRoundStart()
+  {
+    this._smokeCount = 0;
+    BattleUnitBuf smoke = base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+    if (smoke != null)
+    {
+      base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Binding, smoke.stack, base.owner);
+      this._smokeCount = smoke.stack;
+    }
+  }
+
+  public override void BeforeGiveDamage(BattleDiceBehavior behavior)
+  {
+    behavior.ApplyDiceStatBonus(new DiceStatBonus
+    {
+      breakDmg = this._smokeCount
+    });
+  }
+}
