f6441e852111b66b456d6fa0d5d40a6da54d1f14
TundraClimate <tundra.tundere@gmail.com>
2023-11-20 07:10:04 +0900

    feat: added SmokeSupply Passive

---
+++ SmokePack/PassiveAbility_SmokeSupply.cs
@@ -0,0 +1,7 @@
+﻿public class PassiveAbility_SmokeSupply : PassiveAbilityBase
+{
+  public override void OnRoundStart()
+  {
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Smoke, 1, base.owner);
+  }
+}
