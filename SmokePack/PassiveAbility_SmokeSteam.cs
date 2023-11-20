c1d3d644d344d2ca32ec41ccca6d7348a940a0b3
TundraClimate <tundra.tundere@gmail.com>
2023-11-21 06:25:32 +0900

    feat: added SmokeSteam Passive

---
+++ SmokePack/PassiveAbility_SmokeSteam.cs
@@ -0,0 +1,13 @@
+﻿public class PassiveAbility_SmokeSteam : PassiveAbilityBase
+{
+  public override void OnRoundStart()
+  {
+    BattleUnitBuf smoke = base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+    int quick = 0;
+    if (smoke == null) return;
+    else quick += 2; 
+    if (smoke.stack >= 6)
+      quick += 2;
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Quickness, quick, base.owner);
+  }
+}
