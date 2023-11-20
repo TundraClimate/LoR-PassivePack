80fb8cbc64f17ca2f5a6d184320c80c9c59a1696
TundraClimate <tundra.tundere@gmail.com>
2023-11-20 20:13:53 +0900

    feat: added SmokeWar Passive

---
+++ SmokePack/PassiveAbility_SmokeWar.cs
@@ -0,0 +1,25 @@
+﻿public class PassiveAbility_SmokeWar : PassiveAbilityBase
+{
+  public override void OnRoundStart()
+  {
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Decay, 1, base.owner);
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Weak, 1, base.owner);
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Disarm, 1, base.owner);
+  }
+
+  public override void OnWaveStart()
+  {
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Decay, 5, base.owner);
+    base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Smoke, 8, base.owner);
+  }
+
+  public override void OnSucceedAttack(BattleDiceBehavior behavior)
+  {
+    BattleUnitModel target = behavior.card.target;
+    if (target == null) return;
+    if (base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke) != null)
+    {
+      target.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Decay, 1, base.owner);
+    }
+  }
+}
