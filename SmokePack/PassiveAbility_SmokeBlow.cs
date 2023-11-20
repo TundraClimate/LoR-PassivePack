2ea32d222ba97e39d5ad06f3e89fbe91ed434bd2
TundraClimate <tundra.tundere@gmail.com>
2023-11-20 23:17:24 +0900

    feat: added SmokeBlow Passive

---
+++ SmokePack/PassiveAbility_SmokeBlow.cs
@@ -0,0 +1,20 @@
+﻿using LOR_DiceSystem;
+
+public class PassiveAbility_SmokeBlow : PassiveAbilityBase
+{
+  public override void OnUseCard(BattlePlayingCardDataInUnitModel curCard)
+  {
+    List<DiceBehaviour> dices = curCard.GetOriginalDiceBehaviorList();
+    if (dices.FindAll(dice => dice.Type != BehaviourType.Standby).Count == 1)
+    {
+      BattleUnitBuf smoke = base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+      if (smoke != null)
+      {
+        curCard.ApplyDiceStatBonus(DiceMatch.AllDice, new DiceStatBonus
+        {
+          dmg = smoke.stack * 2
+        });
+      }
+    }
+  }
+}
