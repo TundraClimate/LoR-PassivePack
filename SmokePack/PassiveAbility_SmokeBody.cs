f6db146ab1f7da2a40c32ff536ac12850ea74574
TundraClimate <tundra.tundere@gmail.com>
2023-11-22 16:19:50 +0900

    feat: added SmokeBody Passive

---
+++ SmokePack/PassiveAbility_SmokeBody.cs
@@ -0,0 +1,11 @@
+﻿public class PassiveAbility_SmokeBody : PassiveAbilityBase
+{
+  public override void OnTakeDamageByAttack(BattleDiceBehavior atkDice, int dmg)
+  {
+    if (atkDice != null && !atkDice.owner.IsDead())
+    {
+      BattleUnitModel target = atkDice.owner;
+      target.bufListDetail.AddKeywordBufThisRoundByEtc(KeywordBuf.Smoke, 1);
+    }
+  }
+}
