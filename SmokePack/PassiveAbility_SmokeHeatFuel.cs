6f19708760c4275439bb30a4da6f4fe3291c7df1
TundraClimate <tundra.tundere@gmail.com>
2023-11-22 16:40:40 +0900

    feat: added SmokeHeatFuel Passive

---
+++ SmokePack/PassiveAbility_SmokeHeatFuel.cs
@@ -0,0 +1,13 @@
+﻿public class PassiveAbility_SmokeHeatFuel : PassiveAbilityBase
+{
+  public override void OnRoundEnd()
+  {
+    BattleUnitBuf smoke = base.owner.bufListDetail.GetActivatedBuf(KeywordBuf.Smoke);
+    if (smoke != null)
+    {
+      base.owner.bufListDetail.AddKeywordBufByEtc(KeywordBuf.Burn, smoke.stack, base.owner);
+      base.owner.cardSlotDetail.RecoverPlayPoint(smoke.stack / 2);
+      base.owner.bufListDetail.RemoveBuf(smoke);
+    }
+  }
+}
